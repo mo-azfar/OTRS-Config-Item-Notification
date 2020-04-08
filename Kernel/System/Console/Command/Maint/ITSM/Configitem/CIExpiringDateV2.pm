734a61ff9b2fe64b7466d7872511ef43dd1a60d3
Mohd Azfar <62957738+mo-azfar@users.noreply.github.com>
2020-04-09 00:47:11 +0800

    Update CIExpiringDateV2.pm

--- Kernel/System/Console/Command/Maint/ITSM/Configitem/CIExpiringDateV2.pm
+++ Kernel/System/Console/Command/Maint/ITSM/Configitem/CIExpiringDateV2.pm
@@ -1,5 +1,4 @@
-# --
-# Copyright (C) 2001-2018 OTRS AG, https://otrs.com/
+#
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
